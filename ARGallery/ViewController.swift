ed0b22a0688bcf040bcdccdb2e5eabd120a87c78
vladgrudak <ralkavladislav@gmail.com>
2020-04-24 00:48:20 +0300

    Initial Commit

---
+++ ARGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARGallery
+//
+//  Created by Владислав Ралка on 24.04.2020.
+//  Copyright © 2020 Grudak team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
